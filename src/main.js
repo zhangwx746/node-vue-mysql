a67dad477c1a4cc8add675fd0a45216e6b2108e3
wenxu.zhang <wenxu.zhang@langtaojin.com>
2019-03-19 11:21:18 +0800

    '修改组件按需加载，优化打包大小'

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import router from './router'
-import ElementUI from 'element-ui'
+import { Button, Input, Row, Col } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI)
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Row)
+Vue.use(Col)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  ElementUI,
   render: h => h(App)
 })
